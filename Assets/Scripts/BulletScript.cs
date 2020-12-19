f123d77a89eca5527d0fbcae1baaa2237daf4363
Fisnik Krasniqi <uk046538@student.uni-kassel.de>
2020-12-19 20:04:36 +0100

    added crosshair, bullet to shoot with left mouse button, some weapon sprites, collision detection with enemy entity

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+
+     public float bulletSpeed = 60.0f;
+     public float damage = 3.0f;
+     private Rigidbody2D rb;
+     private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(!this.GetComponent<Renderer>().isVisible){
+                Destroy(this.gameObject);
+        }
+
+        
+    }
+
+
+      private void OnTriggerEnter2D(Collider2D other){
+    
+        if(other.tag == "Enemy"){
+          
+           other.gameObject.GetComponent<Entity>().takeDamage(damage);
+           Destroy(this.gameObject);
+        }
+    }
+
+ 
+}
---
+++ Assets/Scripts/CrosshairMouseScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairMouseScript : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject bulletPrefab;
+    public GameObject bulletStart;
+    
+    public float bulletSpeed = 60.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseCursor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mouseCursor;
+        Vector3 crosshairPlayerDifference = transform.position - bulletStart.transform.position;
+
+        float rotationZ = Mathf.Atan2(crosshairPlayerDifference.y, crosshairPlayerDifference.x) * Mathf.Rad2Deg;
+
+        if(Input.GetMouseButtonDown(0)){
+
+            float distance = crosshairPlayerDifference.magnitude;
+            Vector2 direction = crosshairPlayerDifference / distance;
+            direction.Normalize();
+            fireBullet(direction, rotationZ);
+
+        }
+
+    }
+
+     void fireBullet(Vector2 direction, float rotationZ){
+        GameObject b = Instantiate(bulletPrefab) as GameObject;
+        b.transform.position = bulletStart.transform.position;
+        b.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        b.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+     
+    }
+
+}
