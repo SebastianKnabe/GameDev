2b1d303c32440d4062baeb36a13b8eb160d92e62
SebastianKnabe <sekna@t-online.de>
2021-03-18 12:05:17 +0100

    enhance ui

---
+++ Assets/Scripts/Inventory/TargetInventoryUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TargetInventoryUI : MonoBehaviour
+{
+    [SerializeField] Inventory targetInventory = null;
+    [SerializeField] private TextMeshProUGUI targetInventoryLabel = null;
+
+
+    private void OnEnable()
+    {
+        targetInventoryLabel.text  = targetInventory.inventoryType.ToString();
+    }
+}
