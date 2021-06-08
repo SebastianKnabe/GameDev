865f55271242f063a83fd3b33fa8fd9ec6c30c3c
Christopher <christopher.bartalos@clarilab.de>
2021-06-08 09:10:46 +0200

    dev: adds new obstacles

---
+++ Assets/DispenserScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispenserScript : MonoBehaviour
+{
+
+    public TriggerObstacle obstaclePrefab;
+    public float obstacleSpeed;
+    private float time = 0;
+    public float obstacleDelay;
+
+    private bool shoot;
+
+    private Vector2 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dir = new Vector2(obstacleSpeed, 0);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shoot == true)
+        {
+            if (time < Time.time)
+            {
+                TriggerObstacle obstacle = Instantiate(obstaclePrefab, transform.position, transform.rotation) as TriggerObstacle;
+                obstacle.dir = dir;
+                time = Time.time + obstacleDelay;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            shoot = true;
+        } 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            shoot = false;
+        }
+    }
+}
--- Assets/MoveSpike.cs
+++ Assets/MoveSpike.cs
@@ -2,24 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerObstacle : MonoBehaviour
+public class MoveSpike : MonoBehaviour
 {
 
-    Rigidbody2D rb;
+    private bool move;
     public Vector2 dir = new Vector2(0, 0);
-
-
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        rb.velocity = dir;
+        
     }
+
 }
