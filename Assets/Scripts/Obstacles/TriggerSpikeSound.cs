0917a4ef1c1f7bd4bc4924f60373e03297b24cdd
Christopher <christopher.bartalos@clarilab.de>
2021-06-29 11:56:26 +0200

    dev: adds sound to spikes when they go up

---
+++ Assets/Scripts/Obstacles/TriggerSpikeSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSpikeSound : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    [SerializeField] private AudioClip spikeSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlaySpikeSound()
+    {
+        audioSource.PlayOneShot(spikeSound);
+    }
+}
