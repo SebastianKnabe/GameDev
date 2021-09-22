6c6b11a59c8d7e952bede30ea1edb9f9db924520
Christopher <christopher.bartalos@clarilab.de>
2021-09-22 20:17:39 +0200

    dev: adds parallax to earth and adds vertical parallax to script

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -6,30 +6,43 @@ public class Parallax : MonoBehaviour
 {
     [SerializeField] private GameObject camera;
     [SerializeField] private float paralaxxEffect;
+    public bool useYAxis = false; 
 
     private float length;
+    private float height;
     private float startposX;
+    private float startposY;
 
     void Start()
     {
         startposX = transform.position.x;
+        startposY = transform.position.y;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+        height = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     void Update()
     {
-        float temp = (camera.transform.position.x * (1 - paralaxxEffect));
-        float distance = camera.transform.position.x * paralaxxEffect;
+        float tempX = (camera.transform.position.x * (1 - paralaxxEffect));
+        float distanceX = camera.transform.position.x * paralaxxEffect;
+        float tempY = (camera.transform.position.y * (1 - paralaxxEffect));
+        float distanceY = (camera.transform.position.y * (1 - paralaxxEffect));
 
-        transform.position = new Vector3(startposX + distance, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startposX + distanceX, useYAxis ? startposY + distanceY : startposY, transform.position.z);
 
-        if (temp > startposX + length * 0.75f)
+        if (tempX > startposX + length * 0.75f)
         {
             startposX += length;
         }
-        else if (temp < startposX - length * 0.75f)
+        else if (tempX < startposX - length * 0.75f)
         {
             startposX -= length;
+        } else if (useYAxis && tempY < startposY - height * 0.75f)
+        {
+            startposY -= height;
+        } else if (useYAxis && tempY > startposY + height * 0.75f)
+        {
+            startposY += height;
         }
     }
 }
