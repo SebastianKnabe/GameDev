7d7a6b1c48191366d1cc5c6222cf56312bec2bf8
Christopher <chrisibartalos96@web.de>
2021-10-28 16:28:03 +0200

    dev: adds torch

---
+++ Assets/TorchFlicker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class TorchFlicker : MonoBehaviour
+{
+
+    private Light2D light;
+
+    private void Start()
+    {
+        light = GetComponent<Light2D>();
+        InvokeRepeating("flicker", 1f, 1f);
+
+    }
+
+    private void flicker()
+    {
+        light.intensity = Random.Range(0.5f, 2.9f);
+    }
+}
