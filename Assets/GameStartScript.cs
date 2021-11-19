eea15286a2582be09abb66492075912490f1ffa4
Christopher <chrisibartalos96@web.de>
2021-11-19 14:20:45 +0100

    dev: fix textposition game start

--- Assets/GameStartScript.cs
+++ Assets/GameStartScript.cs
@@ -1,21 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStartScript : MonoBehaviour
 {
 
-    [SerializeField] private GameObject TextObject;
-    [SerializeField] private GameObject TextPosition;
+    [SerializeField] private TextMeshProUGUI textObject;
 
     private GameObject instanceOfTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        TextObject.GetComponent<TextMesh>().text = "After a long journey you find yourself back on your home planet, but everything changed.\n Find out what happened. It is also not very helpful that you ship ran out of fuel, find some!";
-        instanceOfTextObject = Instantiate(TextObject, TextPosition.transform.position, Quaternion.identity, TextPosition.transform);
-        instanceOfTextObject.GetComponent<TextMesh>().characterSize = 0.12f;
+        textObject.SetText("After a long journey you find yourself back on your home planet, but everything changed. Find out what happened. It is also not very helpful that you ship ran out of fuel, find some!");
         StartCoroutine(showStartText());
 
     }
@@ -29,7 +27,7 @@ public class GameStartScript : MonoBehaviour
             yield return 0;
 
         }
-        Destroy(instanceOfTextObject);
+        textObject.gameObject.SetActive(false);
         Time.timeScale = 1;
 
     }
