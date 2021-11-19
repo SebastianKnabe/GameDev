db6dd6993d5c0e44fe4ee80f0241a6a2406b23b5
Christopher <chrisibartalos96@web.de>
2021-11-19 13:49:00 +0100

    fix: more fixes

--- Assets/GameStartScript.cs
+++ Assets/GameStartScript.cs
@@ -15,7 +15,7 @@ public class GameStartScript : MonoBehaviour
     {
         TextObject.GetComponent<TextMesh>().text = "After a long journey you find yourself back on your home planet, but everything changed.\n Find out what happened. It is also not very helpful that you ship ran out of fuel, find some!";
         instanceOfTextObject = Instantiate(TextObject, TextPosition.transform.position, Quaternion.identity, TextPosition.transform);
-        instanceOfTextObject.GetComponent<TextMesh>().characterSize = 0.18f;
+        instanceOfTextObject.GetComponent<TextMesh>().characterSize = 0.12f;
         StartCoroutine(showStartText());
 
     }
